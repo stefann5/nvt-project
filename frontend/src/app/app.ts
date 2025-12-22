88586238c3513b9114facffb8ae86de3e1eb36ab
computer <stefan.nik04@gmail.com>
2025-12-22 18:30:15 +0100

    initialized frontend

---
+++ frontend/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-root',
+  imports: [ButtonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('frontend');
+}
